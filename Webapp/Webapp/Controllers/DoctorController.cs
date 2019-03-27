35bd63264bf0c9a6fdd6440cd095e1ec88fb1a51
Wouw,Kevin K.A.P. van der <kevin.vanderwouw@student.fontys.nl>
2019-03-27 09:35:46 +0100

    arts ziet patientgegevens in

--- Webapp/Webapp/Controllers/DoctorController.cs
+++ Webapp/Webapp/Controllers/DoctorController.cs
@@ -14,62 +14,7 @@ namespace Webapp.Controllers
         {
             List<Doctor> items = new List<Doctor>()
             {
-                new Doctor()
-                {
-                    Name = "Harm Hardloper",
-                    Function = "Cardioloog"
-
-                },
-                new Doctor()
-                {
-                    Name = "Gerda Grottenfuhler",
-                    Function = "Gynaecoloog"
-                },
-                new Doctor()
-                {
-                    Name = "Klaas Koppijn",
-                    Function = "Hersenchirurg"
-                },
-                new Doctor()
-                {
-                    Name = "Sonya Slaapmaardoor",
-                    Function = "Anesthesist"
-                },
-                new Doctor()
-                {
-                    Name = "Floor Flamoes",
-                    Function = "Gynaecoloog"
-                },
-                new Doctor()
-                {
-                    Name = "Karel van Kamelenrug",
-                    Function = "Mammografist"
-                },
-                new Doctor()
-                {
-                    Name = "Aaron van Astma",
-                    Function = "Longexpert"
-                },
-                new Doctor()
-                {
-                    Name = "Peter Plasman",
-                    Function = "Uroloog"
-                },
-                new Doctor()
-                {
-                    Name = "Simon Spraakgebrek",
-                    Function = "Logopedist"
-                },
-                new Doctor()
-                {
-                    Name = "Olga Ongelukya-inyabroek'ya",
-                    Function = "Uroloog"
-                },
-                new Doctor()
-                {
-                    Name = "Karel Kinderlach",
-                    Function = "Cliniclown"
-                },
+               
             };
             return View(items);
         }
