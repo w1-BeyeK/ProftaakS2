8f3922187da79a0fe7b3bbbe6ebe498136e1a0c1
Stijn Driessens <s.driessens@student.fontys.nl>
2019-03-27 13:25:54 +0100

    AddViewModels for treatments

---
+++ Webapp/Webapp/Models/TreatmentDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapp.Models
+{
+    public class TreatmentDetailViewModel
+    {
+        public string Name { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        //public TreatmentType TreatmentType { get; set; }
+        //public IEnumerable<Comment> Comments { get; set; }
+    }
+}
---
+++ Webapp/Webapp/Models/TreatmentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapp.Models
+{
+    public class TreatmentViewModel
+    {
+        List<TreatmentDetailViewModel> treatments = new List<TreatmentDetailViewModel>();
+    }
+}
