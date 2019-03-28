e08a139a0a0888ee4d533d598ec5a8973de0070a
Wouw,Kevin K.A.P. van der <kevin.vanderwouw@student.fontys.nl>
2019-03-28 10:50:14 +0100

    treatment detail view model aangepast

--- Webapp/Webapp/Models/TreatmentDetailViewModel.cs
+++ Webapp/Webapp/Models/TreatmentDetailViewModel.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Webapp.Models.Data;
 
 namespace Webapp.Models
 {
     public class TreatmentDetailViewModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string TreatmentName { get; set; }
+        public TreatmentType TreatmentType { get; set; }
         public DateTime BeginDate { get; set; }
+        public DateTime BeginTime { get; set; }
         public DateTime EndDate { get; set; }
-        public long PatientId { get; set; }
-        public string PatientName { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Comments { get; set; }
+        public string Description { get; set; }
     }
 }
