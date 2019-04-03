9e1cd36583e8a65cdcfde40a2af23cc7a20cbe74
Wouw,Kevin K.A.P. van der <kevin.vanderwouw@student.fontys.nl>
2019-04-03 10:45:13 +0200

    extra converter om behandeling van een patient in te zien

---
+++ Webapp/Webapp/Converters/TreatmentDetailViewModelConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webapp.Models;
+using Webapp.Models.Data;
+
+namespace Webapp.Converters
+{
+    public class TreatmentDetailViewModelConverter
+    {
+        public Patient ViewModelToTreatment(PatientDetailViewModel vm)
+        {
+            Patient patient = new Patient()
+            {
+                Id = vm.Id,
+                Name = vm.Name
+            };
+
+            Treatment treatment = new Treatment()
+            {
+                Id = vm.TreatmentDetailViewModel.Id,
+                Name = vm.TreatmentDetailViewModel.Name,
+                Patient = patient,
+                BeginDate = vm.TreatmentDetailViewModel.BeginDate,
+                EndDate = vm.TreatmentDetailViewModel.EndDate,
+                TreatmentType = vm.TreatmentDetailViewModel.Type
+            };
+
+            patient.AddTreatment(treatment);
+
+            return patient;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="patient"></param>
+        /// <returns></returns>
+        public PatientDetailViewModel PatientToViewModel(Patient patient)
+        {
+            TreatmentDetailViewModel treatmentDetailViewModel = new TreatmentDetailViewModel()
+            {
+                Id = patient.Treatments.First().Id,
+                Name = patient.Treatments.First().Name,
+                Type = patient.Treatments.First().TreatmentType,
+                BeginDate = patient.Treatments.First().BeginDate,
+                EndDate = patient.Treatments.First().EndDate
+            };
+
+            PatientDetailViewModel vm = new PatientDetailViewModel()
+            {
+                Id = patient.Id,
+                Name = patient.Name,
+                TreatmentDetailViewModel = treatmentDetailViewModel
+            };
+            
+            return vm;
+        }
+    }
+}
