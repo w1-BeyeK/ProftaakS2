94aeedb20a3c54741ca19da235630b5e5ae48651
Tim <timvddussen@gmail.com>
2019-04-11 15:03:25 +0200

    Base test context items

--- Webapp/Webapp/Context/BaseTestContext.cs
+++ Webapp/Webapp/Context/BaseTestContext.cs
@@ -36,6 +36,31 @@ namespace Webapp.Context
                 new Comment("Arm afzagen succes2", "De arm is afgezaagd3", DateTime.Today)
             };
 
+            departments = new List<Department>()
+            {
+                new Department()
+                {
+                    Id = 1,
+                    Name = "Soepderpoep",
+                    Description = "Hier wordt soep gemaakt",
+                    Active = true,
+                },
+                new Department()
+                {
+                    Id = 2,
+                    Name = "Toedoe",
+                    Description = "Hier worden dingen nog gedaan",
+                    Active = true,
+                },
+                new Department()
+                {
+                    Id = 3,
+                    Name = "Slaapbank",
+                    Description = "Hier slaapt de slapende slaper",
+                    Active = true,
+                },
+            };
+
             treatments = new List<Treatment>()
             {
                 new Treatment()
@@ -46,6 +71,28 @@ namespace Webapp.Context
                     EndDate = DateTime.Now,
                     Comments = comments,
                     TreatmentType = new TreatmentType("Arm afzagen", "Arm gaat eraf"),
+                    Doctor = new Doctor(11, "jan", "jan@hotmail.com", "Jan"),
+                    Patient = new Patient(12, "kevinbeye", "kevin.beye1999@hotmail.com", "Kevin Beye"),
+                },
+                new Treatment()
+                {
+                    Name = "Swiepen",
+                    Id = 2,
+                    BeginDate = new DateTime(2019, 04, 01, 11, 32, 21),
+                    EndDate = DateTime.Now,
+                    Comments = comments,
+                    TreatmentType = new TreatmentType("SmeerSmaren", "Arm gaat erin"),
+                    Doctor = new Doctor(11, "jan", "jan@hotmail.com", "Jan"),
+                    Patient = new Patient(12, "kevinbeye", "kevin.beye1999@hotmail.com", "Kevin Beye"),
+                },
+                new Treatment()
+                {
+                    Name = "HersenReset",
+                    Id = 3,
+                    BeginDate = new DateTime(2019, 04, 01, 11, 32, 21),
+                    EndDate = DateTime.Now,
+                    Comments = comments,
+                    TreatmentType = new TreatmentType("Formateren", "Die Dikke reboot"),
                     Doctor = new Doctor(11, "jan", "jan@hotmail.com", "Jan"),
                     Patient = new Patient(12, "kevinbeye", "kevin.beye1999@hotmail.com", "Kevin Beye"),
                 }
@@ -136,10 +183,30 @@ namespace Webapp.Context
                     Birth = DateTime.Now,
                     EmployeeNumber = 11,
                     Function = "HOI",
+                    Gender = Gender.Other,
+                    Password = "Test123",
+                    PhoneNumber = "12345"
+                },
+                new Doctor(12, "Soepeeeee", "sssssss@hotmail.com", "Soep")
+                {
+                    Active = true,
+                    Birth = DateTime.Now,
+                    EmployeeNumber = 11,
+                    Function = "HOI",
                     Gender = Gender.Male,
                     Password = "Test123",
                     PhoneNumber = "12345"
-                }
+                },
+                new Doctor(11, "Hakker", "Hakmoes@hotmail.com", "Hakkie")
+                {
+                    Active = true,
+                    Birth = DateTime.Now,
+                    EmployeeNumber = 11,
+                    Function = "HOI",
+                    Gender = Gender.Female,
+                    Password = "Test123",
+                    PhoneNumber = "12345"
+                },
             };
             treatmentTypes = new List<TreatmentType>()
             {
