72045638d695393c149edfda20f783aa95a7521d
Kevin Beye <kbeye1999@hotmail.com>
2019-04-11 14:10:09 +0200

    Added test contexts to startup - replaced non functional mssql contexts

--- Webapp/Webapp/Startup.cs
+++ Webapp/Webapp/Startup.cs
@@ -43,16 +43,24 @@ namespace Webapp
             });
 
             services.AddScoped<IContext, TestContext>();
-            services.AddScoped<IParser, DataRowParser>();
-            services.AddScoped<IHandler, MSSQLHandler>();
+            services.AddTransient<IParser, DataRowParser>();
+            services.AddTransient<IHandler, MSSQLHandler>();
 
-            services.AddScoped<ITreatmentTypeContext, MSSQLTreatmentTypeContext>();
-            services.AddScoped<IDepartmentContext, MSSQLDepartmentContext>();
-            services.AddScoped<IInstitutionContext, MSSQLInstitutionContext>();
-            services.AddScoped<ICommentContext, MSSQLCommentContext>();
-            services.AddScoped<IDoctorContext, MSSQLDoctorContext>();
-            services.AddScoped<IPatientContext, MSSQLPatientContext>();
-            services.AddScoped<ITreatmentContext, MSSQLTreatmentContext>();
+            //services.AddScoped<ITreatmentTypeContext, MSSQLTreatmentTypeContext>();
+            //services.AddScoped<IDepartmentContext, MSSQLDepartmentContext>();
+            //services.AddScoped<IInstitutionContext, MSSQLInstitutionContext>();
+            //services.AddScoped<ICommentContext, MSSQLCommentContext>();
+            //services.AddScoped<IDoctorContext, MSSQLDoctorContext>();
+            //services.AddScoped<IPatientContext, MSSQLPatientContext>();
+            //services.AddScoped<ITreatmentContext, MSSQLTreatmentContext>();
+
+            services.AddScoped<ITreatmentTypeContext, TreatmentTypeTestContext>();
+            services.AddScoped<IDepartmentContext, DepartmentTestContext>();
+            services.AddScoped<IInstitutionContext, InstitutionTestContext>();
+            services.AddScoped<ICommentContext, CommentTestContext>();
+            services.AddScoped<IDoctorContext, DoctorTestContext>();
+            services.AddScoped<IPatientContext, PatientTestContext>();
+            services.AddScoped<ITreatmentContext, TreatmentTestContext>();
 
             services.AddScoped<PatientRepository>();
             services.AddScoped<TreatmentTypeRepository>();
