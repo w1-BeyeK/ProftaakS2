43aff63b0f549521b9dfa614a39052159ade35df
Wang Zhou <wang-yang9@hotmail.com>
2019-04-11 13:03:04 +0200

    added more unittests

---
+++ Webapp/TestWebapp/TestRepositories/TestCommentRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webapp.Context;
+using Webapp.Interfaces;
+using Webapp.Models.Data;
+using Webapp.Repository;
+using Xunit;
+
+namespace TestWebapp.TestRepositories
+{
+
+    public class TestCommentRepository
+    {
+        ICommentContext context = new CommentTestContext();
+        CommentRepository commentRepository;
+        [Fact]
+        public void CommentRepositoryConstructor()
+        {
+            commentRepository = new CommentRepository(context);
+
+            Assert.NotNull(commentRepository);
+        }
+
+        [Fact]
+        public void Add()
+        {
+            commentRepository = new CommentRepository(context);
+            Comment comment = new Comment();
+
+            Assert.True(commentRepository.Add(comment, 1));
+        }
+
+        [Fact]
+        public void GetByTreatment()
+        {
+            commentRepository = new CommentRepository(context);
+            
+            Assert.Equal(3, commentRepository.GetByTreatment(1).Count);
+        }
+    }
+}
