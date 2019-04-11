dfe5aa66e436fb93cebc8feb512252c375911e7a
Wang Zhou <wang-yang9@hotmail.com>
2019-04-11 13:34:41 +0200

    Added department.Id and more UnitTests

---
+++ Webapp/TestWebapp/TestRepositories/TestPatientRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webapp.Context;
+using Webapp.Interfaces;
+using Webapp.Models.Data;
+using Webapp.Repository;
+using Xunit;
+
+namespace TestWebapp.TestRepositories
+{
+
+    public class TestPatientRepository
+    {
+        IPatientContext context = new PatientTestContext();
+        PatientRepository patientRepository;
+
+        [Fact]
+        public void PatientRepositoryConstructor()
+        {
+            patientRepository = new PatientRepository(context);
+
+            Assert.NotNull(patientRepository);
+        }
+
+        [Fact]
+        public void Add()
+        {
+            patientRepository = new PatientRepository(context);
+
+            Patient patient = new Patient();
+
+            Assert.Equal(1, patientRepository.Add(patient));
+        }
+
+        [Fact]
+        public void GetAll()
+        {
+            patientRepository = new PatientRepository(context);
+
+            Assert.Equal(3, patientRepository.GetAll().Count);
+        }
+
+        [Fact]
+        public void GetById()
+        {
+            patientRepository = new PatientRepository(context);
+
+
+            Assert.Equal("naam", patientRepository.GetById(1).Name);
+        }
+
+        [Fact]
+        public void Update()
+        {
+            patientRepository = new PatientRepository(context);
+
+            Patient patient = new Patient(1, "naam", "beschrijving", "naam");
+
+            Assert.True(patientRepository.Update(patient));
+        }
+    }
+}
