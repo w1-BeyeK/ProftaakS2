5b0c2b9a5cb0cd1147e58f2893e2453f8927a152
Wang Zhou <wang-yang9@hotmail.com>
2019-04-25 11:42:06 +0200

    small change treatment sql query

--- Webapp/Webapp/Context/MSSQLContext/MSSQLTreatmentContext.cs
+++ Webapp/Webapp/Context/MSSQLContext/MSSQLTreatmentContext.cs
@@ -227,7 +227,7 @@ namespace Webapp.Context.MSSQLContext
                 };
 
                 // Set query
-                string query = $"select * from PTS2_Treatment where DoctorId = @doctorId and PatientId = @patientId AND EndDate >= convert(date, {DateTime.Today.AddYears(-1)})";
+                string query = $"select * from PTS2_Treatment where DoctorId = @doctorId and PatientId = @patientId and EndDate >= convert(date, {DateTime.Today.AddYears(-1)})";
 
                 // Tell the handler to execute the query
                 var dbResult = handler.ExecuteSelect(query, parameters) as DataTable;
