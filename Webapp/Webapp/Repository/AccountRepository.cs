dfecbf49e31a59976a00a47eca4d6e2a5cbd50da
Wouw,Kevin K.A.P. van der <kevin.vanderwouw@student.fontys.nl>
2019-05-08 13:16:35 +0200

    account fix

---
+++ Webapp/Webapp/Repository/AccountRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webapp.Context.InterfaceContext;
+using Webapp.Models.Data;
+
+namespace Webapp.Repository
+{
+    public class AccountRepository
+    {
+        protected readonly IAccountContext context;
+
+        public AccountRepository(IAccountContext context)
+        {
+            this.context = context ?? throw new NullReferenceException("De accountContext is leeg.");
+        }
+
+        public UserAccount GetById(long id)
+        {
+            if (id < 1)
+            {
+                throw new NullReferenceException("De accountId is leeg.");
+            }
+            return this.context.GetById(id);
+        }
+        public List<UserAccount> GetAll()
+        {
+            return this.context.GetAll();
+        }
+        public UserAccount GetForDoctor(long id)
+        {
+            if (id < 1)
+            {
+                throw new NullReferenceException("De accountId is leeg.");
+            }
+            return GetAll().FirstOrDefault(a => a.DoctorId == id);
+        }
+        public UserAccount GetForPatient(long id)
+        {
+            if (id < 1)
+            {
+                throw new NullReferenceException("De accountId is leeg.");
+            }
+            return GetAll().FirstOrDefault(a => a.PatientId == id);
+        }
+        public long Insert(UserAccount obj)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException("De account is leeg.");
+            }
+            return this.context.Insert(obj);
+        }
+        public bool Update(UserAccount obj)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException("De account is leeg.");
+            }
+            return this.context.Update(obj);
+        }
+    }
+}
