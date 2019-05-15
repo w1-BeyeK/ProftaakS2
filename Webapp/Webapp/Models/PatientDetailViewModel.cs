1338765e1ebadce0b45473eaf9a7002aedd707ae
Stijn Driessens <s.driessens@student.fontys.nl>
2019-05-15 11:03:46 +0200

    Fix ViewModels (Except Doctor)

--- Webapp/Webapp/Models/PatientDetailViewModel.cs
+++ Webapp/Webapp/Models/PatientDetailViewModel.cs
@@ -13,19 +13,20 @@ namespace Webapp.Models
         [Range(1, long.MaxValue, ErrorMessage = "PatiëntId is niet binnen de grenzen van 1 en 9223372036854775807")]
         public long Id { get; set; }
         [Required]
-        [StringLength(200, ErrorMessage = "{0} length must be between {1} and {200}.", MinimumLength = 1)]
+        [StringLength(200, ErrorMessage = "Username length must be between 1 and 200.", MinimumLength = 1)]
         public string UserName { get; set; }
         [Required]
-        [StringLength(200, ErrorMessage = "{0} length must be between {1} and {200}.", MinimumLength = 1)]
+        [StringLength(200, ErrorMessage = "Email length must be between 1 and 200.", MinimumLength = 1)]
         public string Email { get; set; }
         [Required]
-        [StringLength(200, ErrorMessage = "{0} length must be between {1} and {200}.", MinimumLength = 1)]
+        [StringLength(200, ErrorMessage = "Name length must be between 1 and 200.", MinimumLength = 1)]
         public string Name { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
         public DateTime Birth { get; set; }
-        [StringLength(15, ErrorMessage = "{0} telefoonnummer is tussen de {7} en {15} tekens lang.", MinimumLength = 7)]
+
+        [StringLength(15, ErrorMessage = "PhoneNumber telefoonnummer is tussen de 7 en 15 tekens lang.", MinimumLength = 7)]
         public string PhoneNumber { get; set; }
         [Required]
         public Gender Gender { get; set; }
@@ -35,11 +36,11 @@ namespace Webapp.Models
         public long BSN { get; set; }
 
         [Required]
-        [StringLength(200, ErrorMessage = "{0} length must be between {1} and {200}.", MinimumLength = 1)]
+        [StringLength(200, ErrorMessage = "ContactPersonName length must be between 1 and 200.", MinimumLength = 1)]
         public string ContactPersonName { get; set; }
 
         [Required]
-        [StringLength(200, ErrorMessage = "{0} length must be between {1} and {200}.", MinimumLength = 1)]
+        [StringLength(200, ErrorMessage = "ContactPersonPhone length must be between 1 and 200.", MinimumLength = 1)]
         [DataType(DataType.PhoneNumber)]
         public string ContactPersonPhone { get; set; }
         public int HouseNumber { get; set; }
