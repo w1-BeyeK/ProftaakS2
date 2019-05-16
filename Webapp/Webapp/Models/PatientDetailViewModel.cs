c7f6007e0bb2374c9a7710701e56cd7d1d9f3163
Stijn Driessens <s.driessens@student.fontys.nl>
2019-05-16 10:26:29 +0200

    Add Displays to viewmodels

--- Webapp/Webapp/Models/PatientDetailViewModel.cs
+++ Webapp/Webapp/Models/PatientDetailViewModel.cs
@@ -12,47 +12,81 @@ namespace Webapp.Models
         [Required]
         [Range(1, long.MaxValue, ErrorMessage = "PatiëntId is niet binnen de grenzen van 1 en 9223372036854775807")]
         public long Id { get; set; }
+
         [Required]
         [StringLength(200, ErrorMessage = "Username length must be between 1 and 200.", MinimumLength = 1)]
+        [Display(Name = "Gebruikersnaam")]
         public string UserName { get; set; }
+
         [Required]
         [StringLength(200, ErrorMessage = "Email length must be between 1 and 200.", MinimumLength = 1)]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
         [Required]
         [StringLength(200, ErrorMessage = "Name length must be between 1 and 200.", MinimumLength = 1)]
+        [Display(Name = "Naam")]
         public string Name { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
+        [Display(Name = "Geboortedatum")]
         public DateTime Birth { get; set; }
 
         [StringLength(15, ErrorMessage = "Phone telefoonnummer is tussen de 7 en 15 tekens lang.", MinimumLength = 7)]
+        [Display(Name = "Telefoonnummer")]
         public string Phone { get; set; }
+
         [Required]
+        [Display(Name = "Geslacht")]
         public Gender Gender { get; set; }
 
         [Required]
         [Range(1, long.MaxValue, ErrorMessage = "BSN is niet binnen de grenzen van 1 en 9223372036854775807")]
+        [Display(Name = "BSN")]
         public long BSN { get; set; }
 
         [Required]
         [StringLength(200, ErrorMessage = "ContactPersonName length must be between 1 and 200.", MinimumLength = 1)]
+        [Display(Name = "Naam van de contactpersoon")]
         public string ContactPersonName { get; set; }
 
         [Required]
         [StringLength(200, ErrorMessage = "ContactPersonPhone length must be between 1 and 200.", MinimumLength = 1)]
         [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Telefoonnummer van de contactpersoon")]
         public string ContactPersonPhone { get; set; }
+
+        [Display(Name = "Huisnummer")]
         public int HouseNumber { get; set; }
+
+        [Display(Name = "Postcode")]
         public string Zipcode { get; set; }
+
+        [Display(Name = "Naam van de contactpersoon zichtbaar maken")]
         public bool PrivContactPersonName { get; set; }
+
+        [Display(Name = "Telefoonnummer van de contactpersoon zichtbaar maken")]
         public bool PrivContactPersonPhone { get; set; }
+
+        [Display(Name = "Email zichtbaar maken")]
         public bool PrivMail { get; set; }
+
+        [Display(Name = "Telefoonnummer zichtbaar maken")]
         public bool PrivPhone { get; set; }
+
+        [Display(Name = "Adres zichtbaar maken")]
         public bool PrivAdress { get; set; }
+
+        [Display(Name = "Geslacht zichtbaar maken")]
         public bool PrivGender { get; set; }
+
+        [Display(Name = "'Geboortedatum zichtbaar maken")]
         public bool PrivBirthDate { get; set; }
+
+        [Display(Name = "Leeftijd zichtbaar maken")]
         public int Age { get; set; }
+
         public List<TreatmentDetailViewModel> TreatmentDetailViewModels { get; set; }
     }
 }
